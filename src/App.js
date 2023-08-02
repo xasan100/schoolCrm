3f38b385ea942d686b0e34f98895d0f8ebf9cc4f
Muhammad3111 <karimovmuhammad3502@gmail.com>
2023-08-02 16:15:53 +0500

    update file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Router from "./router/Router";
 
 function App() {
   return (
-    <div >
-
+    <div>
+      <Router />
     </div>
   );
 }
