ffeb3d53d6f2ce5fd3cedb180c0eb0c89bd1a550
Muhammad3111 <karimovmuhammad3502@gmail.com>
2023-08-11 21:09:28 +0500

    update file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Router from "./router/Router";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Router />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Sigin from '../components/login/sigin/index.jsx'
+import React from "react";
+import Sigin from "../components/login/sigin/index.jsx";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard.jsx";
+import Layout from "../components/layout/Layout.jsx";
 
 export default function Router() {
   return (
-    <div>
-      {<Sigin /> }
-    </div>
-  )
+    <Routes>
+      <Route path="login" element={<Sigin />} />
+      <Route element={<Layout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
+  );
 }
