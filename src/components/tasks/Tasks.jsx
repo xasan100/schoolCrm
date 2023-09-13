a1c261784fc438fe3c638e8713f5b8a04df4f513
Muhammad3111 <karimovmuhammad3502@gmail.com>
2023-09-13 06:15:33 +0500

    update some bugs

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -6,18 +6,8 @@ import ComplateTask from "./ComplateTask";
 import DeleteTask from "./DeleteTask";
 import UpdateTask from "./UpdateTask";
 
-export default function Tasks() {
-  const { data, isLoading } = useGetTasksQuery();
+const TaskCard = ({ task, color, keyword }) => {
   const { data: teachers } = useGetTeachersQuery();
-  const redTasks = isLoading
-    ? []
-    : data.filter((task) => !task.complete_from_user && !task.complete_to_user);
-  const yellowTasks = isLoading
-    ? []
-    : data.filter((task) => task.complete_to_user && !task.complete_from_user);
-  const greenTasks = isLoading
-    ? []
-    : data.filter((task) => task.complete_to_user && task.complete_from_user);
 
   const fintTeacher = (id) => {
     const findObejct = teachers.find((item) => item.id === id);
@@ -26,76 +16,67 @@ export default function Tasks() {
       last_name: findObejct.user.last_name,
     };
   };
+  const { name, last_name } = fintTeacher(task?.to_user);
+
+  return (
+    <div className={`bg-${color}-400 rounded-md shadow-md p-2 group relative`}>
+      <div className="text-white">
+        <h1 className="font-bold">{task?.task_title}</h1>
+        <p>{task?.task_message}</p>
+      </div>
+      <div className="bg-white p-1 w-max rounded-md">
+        <h1>
+          {name} {last_name}
+        </h1>
+      </div>
+      <div className="flex gap-1 absolute top-1 right-1 invisible group-hover:visible duration-150">
+        {color !== "green" && <ComplateTask object={task} keyWord={keyword} />}
+        <DeleteTask ID={task.id} />
+        {color !== "green" && color !== "yellow" && (
+          <UpdateTask object={task} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default function Tasks() {
+  const { data, isLoading } = useGetTasksQuery();
+
+  const filterTasks = (data) => {
+    return {
+      red: data.filter(
+        (task) => !task.complete_from_user && !task.complete_to_user
+      ),
+      yellow: data.filter(
+        (task) => task.complete_to_user && !task.complete_from_user
+      ),
+      green: data.filter(
+        (task) => task.complete_to_user && task.complete_from_user
+      ),
+    };
+  };
+
+  const allTasks = isLoading
+    ? { red: [], yellow: [], green: [] }
+    : filterTasks(data);
+
   return (
     <div className="grid grid-cols-3 h-[78vh] col-span-12">
       <div className="col-span-1 bg-red-100 border-2 border-r-0 p-1 flex flex-col gap-2 overflow-y-scroll">
         <AddTask />
-        {redTasks.map((e) => (
-          <div
-            key={e.id}
-            className="bg-red-500 rounded-md shadow-md p-2 group relative"
-          >
-            <div className="text-white">
-              <h1 className="font-bold">{e?.task_title}</h1>
-              <p>{e?.task_message}</p>
-            </div>
-            <div className="bg-white p-1 w-max rounded-md">
-              <h1>
-                {fintTeacher(e?.to_user).name}{" "}
-                {fintTeacher(e?.to_user).last_name}
-              </h1>
-            </div>
-            <div className="flex gap-1 absolute top-1 right-1 invisible group-hover:visible duration-150">
-              <ComplateTask object={e} keyWord="complete_to_user" />
-              <UpdateTask object={e} />
-              <DeleteTask ID={e.id} />
-            </div>
-          </div>
+        {allTasks.red.map((e) => (
+          <TaskCard task={e} color="red" keyword="complete_to_user" />
         ))}
       </div>
       <div className="col-span-1 bg-yellow-100 border-2 p-1 flex flex-col gap-2 overflow-y-scroll">
-        {yellowTasks.map((e) => (
-          <div
-            key={e.id}
-            className="bg-yellow-500 rounded-md shadow-md p-2 group relative"
-          >
-            <div className="text-white">
-              <h1 className="font-bold">{e?.task_title}</h1>
-              <p>{e?.task_message}</p>
-            </div>
-            <div className="bg-white p-1 w-max rounded-md">
-              <h1>
-                {fintTeacher(e?.to_user).name}{" "}
-                {fintTeacher(e?.to_user).last_name}
-              </h1>
-            </div>
-            <div className="flex gap-1 absolute top-1 right-1 invisible group-hover:visible duration-150">
-              <ComplateTask object={e} keyWord="complete_from_user" />
-              <DeleteTask ID={e.id} />
-            </div>
-          </div>
+        {allTasks.yellow.map((e) => (
+          <TaskCard task={e} color="yellow" keyword="complete_from_user" />
         ))}
       </div>
       <div className="col-span-1 bg-green-100 border-2 border-l-0 p-1 flex flex-col gap-2 overflow-y-scroll">
-        {greenTasks.map((e) => (
-          <div
-            key={e.id}
-            className="bg-green-500 rounded-md shadow-md p-2 group relative"
-          >
-            <div className="text-white">
-              <h1 className="font-bold">{e?.task_title}</h1>
-              <p>{e?.task_message}</p>
-            </div>
-            <div className="bg-white p-1 w-max rounded-md">
-              <h1>
-                {fintTeacher(e?.to_user).name}{" "}
-                {fintTeacher(e?.to_user).last_name}
-              </h1>
-            </div>
-            <div className="flex gap-1 absolute top-1 right-1 invisible group-hover:visible duration-150">
-              <DeleteTask ID={e.id} />
-            </div>
-          </div>
+        {allTasks.green.map((e) => (
+          <TaskCard task={e} color="green" />
         ))}
       </div>
     </div>
