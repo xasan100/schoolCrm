49a02417ef21d72f2adc922930948315bc598246
Muhammad3111 <karimovmuhammad3502@gmail.com>
2023-10-09 22:24:03 +0500

    add validation

--- src/generic/InputField.jsx
+++ src/generic/InputField.jsx
@@ -8,9 +8,10 @@ export default function InputField({
   value,
   autoComplete,
   handleChange,
+  icon,
 }) {
   return (
-    <div className="col-span-1 row-span-1">
+    <div className="col-span-1 row-span-1 relative">
       <label
         htmlFor={id}
         className="block text-sm font-medium leading-6 text-gray-900 w-72"
@@ -27,6 +28,7 @@ export default function InputField({
           onChange={(e) => handleChange(e)}
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
+        {icon}
       </div>
     </div>
   );
