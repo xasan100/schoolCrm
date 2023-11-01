9ac4528e555be44cace9c3e5a1dee9b9e7f74b6a
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-11-02 00:00:28 +0500

    update

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,6 +1,8 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 let url = "https://alcrm.pythonanywhere.com/api/v1/";
-let local = `${window.location.protocol}//${window.location.hostname}:8000`
+let local = `${window.location.protocol}//${window.location.hostname}:8000/api/v1`
+
+
 
 export const api = fetchBaseQuery({
   baseUrl: local,
